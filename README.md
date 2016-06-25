262f341158e242e97b7ea7b2a8df80bd12c65e4d
Donald Subert <helo478@gmail.com>
2016-06-24 20:46:21 -0700

    Created a trivial project

---
+++ README.md
@@ -0,0 +1,7 @@
+# OpenJCiv
+
+An open source implementation of a turn-based 4X game, in the style of a popular title
+
+## Contributors
+
+* Don Subert <helo478@gmail.com>
---
+++ src/com/peak15/openjciv/Application.java
@@ -0,0 +1,8 @@
+package com.peak15.openjciv;
+
+public class Application {
+
+  public static void Main(final String[] args) {
+    System.out.println("Hello, World!");
+  }
+}
